60b755d1ba3a3199b7a342aae5b3551517f68a93
vamsi <75676297+beingOptimusP@users.noreply.github.com>
2021-09-17 16:48:07 +0530

    a

--- contracts/distributer.sol
+++ contracts/distributer.sol
@@ -1,84 +1,16 @@
-//SPDX-License-Identifier: MIT;
-
+//SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.8.4;
 
 import "./adamant.sol";
 
-abstract contract Context {
-    function _msgSender() internal view virtual returns (address payable) {
-        return msg.sender;
-    }
-
-    function _msgData() internal view virtual returns (bytes memory) {
-        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
-        return msg.data;
-    }
-}
-
-abstract contract Ownable is Context {
-    address private _owner;
-
-    event OwnershipTransferred(
-        address indexed previousOwner,
-        address indexed newOwner
-    );
-
-    /**
-     * @dev Initializes the contract setting the deployer as the initial owner.
-     */
-    constructor() internal {
-        address msgSender = _msgSender();
-        _owner = msgSender;
-        emit OwnershipTransferred(address(0), msgSender);
-    }
-
-    /**
-     * @dev Returns the address of the current owner.
-     */
-    function owner() public view virtual returns (address) {
-        return _owner;
-    }
-
-    /**
-     * @dev Throws if called by any account other than the owner.
-     */
-    modifier onlyOwner() {
-        require(owner() == _msgSender(), "Ownable: caller is not the owner");
-        _;
-    }
-
-    /**
-     * @dev Leaves the contract without owner. It will not be possible to call
-     * `onlyOwner` functions anymore. Can only be called by the current owner.
-     *
-     * NOTE: Renouncing ownership will leave the contract without an owner,
-     * thereby removing any functionality that is only available to the owner.
-     */
-    function renounceOwnership() public virtual onlyOwner {
-        emit OwnershipTransferred(_owner, address(0));
-        _owner = address(0);
-    }
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Can only be called by the current owner.
-     */
-    function transferOwnership(address newOwner) public virtual onlyOwner {
-        require(
-            newOwner != address(0),
-            "Ownable: new owner is the zero address"
-        );
-        emit OwnershipTransferred(_owner, newOwner);
-        _owner = newOwner;
-    }
-}
-
 contract dis is Ownable {
-    MAP inter = MAP(0xd9145CCE52D386f254917e481eB44e9943F39138);
+    address public addy;
 
-    uint256 fees = 100;
+    Adamant inter = Adamant(0x8CCB7d516ccd62312B2D7753B1421ed3862ff80b);
 
-    address MarAdd = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
+    uint256 fees = 100 * 10**9;
+
+    address MarAdd = 0x43DacA63Af2dF0af230A5497ce319549B1a90ED7;
 
     function updateFees(uint256 _fee) public onlyOwner {
         fees = _fee;
